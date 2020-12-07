028df1d06b518dcdeeaf3d392cb57b16347a11c4
Bharanidhar Reddy <31667768+ABharanidhar@users.noreply.github.com>
2020-12-06 22:26:38 -0600

    last minute changes-fixed clearing cart

--- routes/purchasehistory.js
+++ routes/purchasehistory.js
@@ -4,7 +4,10 @@ var mongoose = require("mongoose");
 
 var pHistory = require("../models/purchasehistory");
 var auth = require("../middleware/auth");
-const { log } = require("debug");
+
+var cart = require("../models/cart");
+
+const Cart = mongoose.model("Cart", cart.cart);
 
 const PurchaseHistory = mongoose.model(
   "PurchaseHistory",
@@ -28,14 +31,27 @@ router.get("/", auth, function (req, res, next) {
 
 router.route("/save").post((req, res, next) => {
   console.log(req.body._data);
-  //let ress = JSON.stringify(req.body);
+  let ress = req.body._data;
   //console.log("aaa" + ress);
 
   PurchaseHistory.create(req.body._data, (error, data) => {
     if (error) {
       return next(error);
     } else {
-      console.log(data);
+      console.log(ress);
+
+      Cart.findOneAndUpdate(
+        { userId: ress.userId },
+        {
+          products: [],
+        },
+        (err) => {
+          if (err) {
+            res.status(500).json({ errorMessage: err });
+          }
+        }
+      );
+
       res.json(data);
     }
   });
